b30e9f427983d62b3d16f67d00809b7ff389384e
Ryuuz02 <ryuuz02@outlook.com>
2022-02-05 00:38:09 -0600

    WIP Converting chess pieces to classes on the board

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class ChessBoard:
             self.board.append([])
         for col in range(0, cols):
             for row in range(0, rows):
-                self.board[col].append("_")
+                self.board[col].append("")
         for row in range(1, rows + 1):
             if row == 1 or row == 8:
                 unit = ""
@@ -31,67 +31,111 @@ class ChessBoard:
                             unit = "Q"
                         elif col == 4:
                             unit = "K"
-                    if row == 1:
-                        self.replace_spot(unit, col, row)
-                    elif row == 8:
-                        self.replace_spot(unit, col, row)
+                    self.replace_spot(unit, col, row)
             elif row == 2:
                 for col in range(1, 9):
                     self.replace_spot("P", col, row)
             elif row == 7:
                 for col in range(1, 9):
                     self.replace_spot("P", col, row)
+            else:
+                for col in range(1, 9):
+                    self.replace_spot("_", col, row)
 
     def print_board(self):
+        a_to_h = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        print("   " + " ".join(a_to_h))
         for row in range(0, self.rows):
             current_line = self.board[row]
-            print("|" + "|".join(current_line) + "|")
+            if row in [0, 1]:
+                print_line = [piece.print() for piece in current_line]
+                print(Fore.RESET + str(8 - row) + Fore.GREEN + " |" + "|".join(print_line) + "|")
+            elif row in [6, 7]:
+                print(Fore.RESET + str(8 - row) + Fore.BLUE + " |" + "|".join(current_line) + "|")
+            else:
+                print(Fore.RESET + str(8 - row) + " |" + "|".join(current_line) + "|")
 
     def replace_spot(self, new_piece, x, y):
         index_accounted_y = y - self.cols
         index_accounted_x = x - self.rows
-        self.board[-index_accounted_y][index_accounted_x] = new_piece
+        piece = dict_of_pieces[new_piece](Fore.BLUE, x, y)
+        self.board[-index_accounted_y][index_accounted_x] = piece
 
 
 class ChessPiece:
-    def __init__(self, symbol, name):
+    def __init__(self, symbol, color, x, y):
+        self.x = x
+        self.y = y
         self.symbol = symbol
-        self.name = name
+        self.color = color
+
+    def move(self):
+        pass
+
+    def print(self):
+        return self.symbol
+
+
+class Empty(ChessPiece):
+    def __init__(self, color, x, y):
+        super().__init__("_", Fore.RESET, x, y)
 
 
 class Pawn(ChessPiece):
-    def __init__(self, name):
-        super().__init__("P", name)
+    def __init__(self, color, x, y):
+        super().__init__("P", color, x, y)
+
+    def move(self):
+        pass
 
 
 class Rook(ChessPiece):
-    def __init__(self):
+    def __init__(self, color, x, y):
+        super().__init__("R", color, x, y)
+
+    def move(self):
         pass
 
 
 class Knight(ChessPiece):
-    def __init__(self):
+    def __init__(self, color, x, y):
+        super().__init__("N", color, x, y)
+
+    def move(self):
         pass
 
 
 class Queen(ChessPiece):
-    def __init__(self):
+    def __init__(self, color, x, y):
+        super().__init__("Q", color, x, y)
+
+    def move(self):
         pass
 
 
 class Bishop(ChessPiece):
-    def __init__(self):
+    def __init__(self, color, x, y):
+        super().__init__("B", color, x, y)
+
+    def move(self):
         pass
 
 
 class King(ChessPiece):
-    def __init__(self):
+    def __init__(self, color, x, y):
+        super().__init__("K", color, x, y)
+
+    def move(self):
         pass
 
 
-def move_piece(piece_symbol):
-    pass
-
-
+dict_of_pieces = {"P": Pawn,
+                  "R": Rook,
+                  "Q": Queen,
+                  "K": King,
+                  "B": Bishop,
+                  "N": Knight,
+                  "_": Empty
+                  }
 chess_board = ChessBoard()
 chess_board.print_board()
